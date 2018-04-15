c2b4d1ad3f0f2dfc1e2b74334b24096f6d313c37
MaryKathryn <16MM79@queensu.ca>
2018-04-15 15:33:52 -0400

    Joystick training task phase 4
    8 different locations the box can appear (left, right, up, down and to all 4 corners). Make sure the fixation point size in the monkeyLogic GUI is 5 deg.

---
+++ joyTrainTask_phase4.m
@@ -0,0 +1,18 @@
+%joystick training task phase 1 
+%timing file
+
+joystickTarget = 1; %So we can remember what this task object is 
+toggleobject(joystickTarget, 'eventmarker', 10, 'status','on'); %show the target, mark this with behavioural code 10
+showcursor(1) %show the joystick cursor on screen 
+
+% [ontarget rt] = eyejoytrack(fxn, object_number, threshold(degrees of visual angle)% how big the invisible target circle is, duration(ms))
+[ontarget rt] = eyejoytrack('acquiretarget', joystickTarget, 3, 5000);   
+if ~ontarget, 
+    trialerror(2); % no or late response (did not land on either the target or distractor) ontarget will return 0
+    toggleobject([joystickTarget])
+    return
+end
+
+trialerror(0); % correct
+goodmonkey(50, 3); % 50ms of juice x 3
+
